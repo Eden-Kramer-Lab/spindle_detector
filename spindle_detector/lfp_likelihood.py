bb48a1dd89983f070e76066d80cda6fc961caa66
Eric Denovellis <edeno@bu.edu>
2019-06-05 15:42:19 -0400

    Allow for multitaper parameters and start time to be set by user

--- spindle_detector/lfp_likelihood.py
+++ spindle_detector/lfp_likelihood.py
@@ -5,6 +5,10 @@ from sklearn.mixture import GaussianMixture
 
 from spectral_connectivity import Connectivity, Multitaper
 
+_DEFAULT_MULTITAPER_PARAMS = dict(time_halfbandwidth_product=1,
+                                  time_window_duration=0.250,
+                                  time_window_step=0.250)
+
 
 def lfp_likelihood(spindle_band_power, spindle_model, no_spindle_model):
     """Estimates the likelihood of being in a spindle state over time given the
@@ -64,13 +68,17 @@ def fit_lfp_likelihood(spindle_band_power, is_spindle,
 
 
 def estimate_spindle_band_power(lfps, sampling_frequency,
-                                spindle_band=(10, 16)):
+                                spindle_band=(10, 16), start_time=0.00,
+                                multitaper_params=_DEFAULT_MULTITAPER_PARAMS):
     """Estimates the spindle power of each LFP.
 
     Parameters
     ----------
     lfps : ndarray, shape (n_time, n_signals)
     sampling_frequency : float
+    spindle_band : (start_freq, end_freq)
+    start_time : float
+    multitaper_params : dict
 
     Returns
     -------
@@ -79,9 +87,7 @@ def estimate_spindle_band_power(lfps, sampling_frequency,
 
     """
     m = Multitaper(lfps, sampling_frequency=sampling_frequency,
-                   time_halfbandwidth_product=1,
-                   time_window_duration=0.250,
-                   time_window_step=0.250)
+                   **multitaper_params)
     c = Connectivity.from_multitaper(m)
     freq_ind = ((c.frequencies > spindle_band[0]) &
                 (c.frequencies < spindle_band[1]))
