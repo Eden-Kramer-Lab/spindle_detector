3dc8564d8e4d2f11dae1eb35630ac089b6e674b5
Eric Denovellis <edeno@bu.edu>
2019-06-05 14:24:59 -0400

    Add files

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+INSTALL_REQUIRES = ['numpy >= 1.14', 'statsmodels', 'numba', 'matplotlib',
+                    'xarray', 'scipy', 'scikit-learn',
+                    'spectral_connectivity', 'hmmlearn']
+TESTS_REQUIRE = ['pytest >= 2.7.1']
+
+setup(
+    name='spindle_detector',
+    version='0.1.0.dev0',
+    license='MIT',
+    description=('Identify spindle events'),
+    author='',
+    author_email='',
+    url='https://github.com/Eden-Kramer-Lab/spindle_detector',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+)
---
+++ spindle_detector/lfp_likelihood.py
@@ -0,0 +1,90 @@
+from functools import partial
+
+import numpy as np
+from sklearn.mixture import GaussianMixture
+
+from spectral_connectivity import Connectivity, Multitaper
+
+
+def lfp_likelihood(spindle_band_power, spindle_model, no_spindle_model):
+    """Estimates the likelihood of being in a spindle state over time given the
+     spectral power of the local field potentials (LFPs).
+
+    Parameters
+    ----------
+    spindle_band_power : ndarray, shape (n_time, n_signals)
+    out_spindle_kde : statsmodels.nonparametric.kernel_density.KDEMultivariate
+    in_spindle_kde : statsmodels.nonparametric.kernel_density.KDEMultivariate
+
+    Returns
+    -------
+    log_likelihood : ndarray, shape (n_time, 2)
+
+    """
+    not_nan = np.all(~np.isnan(spindle_band_power), axis=1)
+    n_time = spindle_band_power.shape[0]
+
+    log_likelihood = np.ones((n_time, 2))
+
+    log_likelihood[not_nan, 0] = no_spindle_model.score_samples(
+        np.log(spindle_band_power[not_nan]))
+
+    log_likelihood[not_nan, 1] = spindle_model.score_samples(
+        np.log(spindle_band_power[not_nan]))
+
+    return log_likelihood
+
+
+def fit_lfp_likelihood(spindle_band_power, is_spindle,
+                       model=GaussianMixture,
+                       model_kwargs=dict(n_components=1)):
+    """Fits the likelihood of being in a spindle state over time given the
+     spectral power of the local field potentials (LFPs).
+
+    Parameters
+    ----------
+    spindle_band_power : ndarray, shape (n_time, n_signals)
+    is_spindle : bool ndarray, shape (n_time,)
+    sampling_frequency : float
+
+    Returns
+    -------
+    likelihood_ratio : function
+
+    """
+
+    not_nan = np.all(~np.isnan(spindle_band_power), axis=1)
+    spindle_model = model(**model_kwargs).fit(
+        np.log(spindle_band_power[is_spindle & not_nan] + np.spacing(1)))
+    no_spindle_model = model(**model_kwargs).fit(
+        np.log(spindle_band_power[~is_spindle & not_nan] + np.spacing(1)))
+
+    return partial(lfp_likelihood, spindle_model=spindle_model,
+                   no_spindle_model=no_spindle_model)
+
+
+def estimate_spindle_band_power(lfps, sampling_frequency,
+                                spindle_band=(10, 16)):
+    """Estimates the spindle power of each LFP.
+
+    Parameters
+    ----------
+    lfps : ndarray, shape (n_time, n_signals)
+    sampling_frequency : float
+
+    Returns
+    -------
+    time : ndarray, shape (n_time_windows,)
+    spindle_band_power : ndarray (n_time_windows, n_signals)
+
+    """
+    m = Multitaper(lfps, sampling_frequency=sampling_frequency,
+                   time_halfbandwidth_product=1,
+                   time_window_duration=0.250,
+                   time_window_step=0.250)
+    c = Connectivity.from_multitaper(m)
+    freq_ind = ((c.frequencies > spindle_band[0]) &
+                (c.frequencies < spindle_band[1]))
+    power = c.power()[..., freq_ind, :].mean(axis=1).squeeze()
+    time = c.time + (m.time_window_duration / 2)
+    return time, power[:, np.newaxis]
